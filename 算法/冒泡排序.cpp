153835b0bea3d8735f86d5b6a03da60ed4efeeb6
RIYI-STAR <2505599817@qq.com>
2025-03-15 14:11:48 +0800

    riyi

---
+++ 算法/冒泡排序.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10] = {5, 3, 8, 6, 2, 7, 1, 4, 9, 0};
+    for (int i = 0; i < 9; i++)
+        for (int j = i; i >= 0; j--)
+            if (arr[j] < arr[j-1])
+                swap(arr[j], arr[j-1]);
+    
+    for (int i = 0; i < 9; i++)
+        cout << arr[i] << " ";
+}
