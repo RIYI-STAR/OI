3b851965a69aceeab221260d2635922e0e1f2e9a
RIYI-STAR <2505599817@qq.com>
2025-04-11 09:24:37 +0800

    4.11

--- LeetCode/字母异位词分组.cpp
+++ LeetCode/字母异位词分组.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
     public:
+        static int hash(string s)
+        {
+            int res = 0;
+            for (char c : s)
+                res += c - 'a' + 1;
+            return res;
+        }
+
         vector<vector<string>> groupAnagrams(vector<string>& strs) {
-            
+            unordered_map<int, vector<string>> mp;
+            for (string& s : strs){
+                int ss = hash(s);
+                mp[ss].push_back(s);
+            }
+            vector<vector<string>> r;
+            for (auto& p : mp)
+                r.push_back(p.second);
+            return r;
         }
     };
+
+// int main() {
+//     Solution s;
+//     vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+//     vector<vector<string>> res = s.groupAnagrams(strs);
+//     for (auto& v : res) {
+//         for (auto& s : v)
+//             cout << s << " ";
+//         cout << endl;
+//     }
+//     return 0;
+// }
