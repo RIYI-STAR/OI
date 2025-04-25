e5e92a23093105dd7f8a20d25fa002f5765c4c23
RIYI-STAR <2505599817@qq.com>
2025-04-25 18:01:50 +0800

    4.25

--- LeetCode/字母异位词分组.cpp
+++ LeetCode/字母异位词分组.cpp
@@ -2,39 +2,49 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
-class Solution {
-    public:
-        static int hash(string s)
+class Solution
+{
+public:
+
+    static int hash(string s)
+    {
+        long long int res = 0;
+        for (char c : s)
+            res = (res * 31 + c) % 1000007;
+        return res;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        vector<vector<string>> res(1000007);
+        for (string &s : strs)
         {
-            int res = 0;
-            for (char c : s)
-                res += c - 'a' + 1;
-            return res;
+            string ss = s;
+            sort(ss.begin(), ss.end());
+            int h = hash(ss);
+            res[h].push_back(s);
         }
+        vector<vector<string>> r;
+        for (auto &v : res)
+            if (!v.empty())
+                r.push_back(v);
+        return r;
+    }
+};
 
-        vector<vector<string>> groupAnagrams(vector<string>& strs) {
-            unordered_map<int, vector<string>> mp;
-            for (string& s : strs){
-                int ss = hash(s);
-                mp[ss].push_back(s);
-            }
-            vector<vector<string>> r;
-            for (auto& p : mp)
-                r.push_back(p.second);
-            return r;
-        }
-    };
-
-// int main() {
-//     Solution s;
-//     vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-//     vector<vector<string>> res = s.groupAnagrams(strs);
-//     for (auto& v : res) {
-//         for (auto& s : v)
-//             cout << s << " ";
-//         cout << endl;
-//     }
-//     return 0;
-// }
+int main()
+{
+    Solution s;
+    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat", "ab", "c"};
+    vector<vector<string>> res = s.groupAnagrams(strs);
+    for (auto &v : res)
+    {
+        for (auto &s : v)
+            cout << s << " ";
+        cout << endl;
+    }
+    return 0;
+}
